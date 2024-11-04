e6e2ab3e2e2e7368422285d3081f7e3da6c99961
rajeshwari211 <krajeshree947@gmail.com>
2024-11-04 13:03:23 +0530

    Add files via upload

--- program5.c
+++ program5.c
@@ -3,11 +3,11 @@ int main()
 {
     int a=2;
     int b=4;
-    int c=a+b;
-    int d=a-b;
-    int e=a/b;
-    int f=a*b;
-    int g=a%b;
-    printf("%d\n,%d\n,%d\n,%d\n,%d/n",c,d,e,f,g);
+    int sum=a+b;
+    int diff=a-b;
+    int div=a/b;
+    int mul=a*b;
+    int mod=a%b;
+    printf("%d\n,%d\n,%d\n,%d\n,%d/n",sum,diff,div,mul,mod);
     return 0;
 }
---
+++ program6.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int a=10;
+    int b=20;
+    if(a<b)
+    {
+        printf("a is lesser than b\n");
+    }
+    else
+    {
+        printf("b is greater that a\n");
+    }
+    if(a==b)
+    {
+        printf("a is equal to b\n");
+    }
+    else{
+        printf("a is not equal to b\n");
+    }
+    return 0;
+}
