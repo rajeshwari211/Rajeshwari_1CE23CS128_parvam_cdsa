4380130c676ecd8defa8f6269dc50bbb9f912d75
rajeshwari211 <krajeshree947@gmail.com>
2024-11-04 12:26:32 +0530

    Add files via upload

---
+++ program4.c
@@ -0,0 +1,14 @@
+# include<stdio.h>
+int main()
+{
+    int a=10;
+    printf("%d\n",a);
+    float b=10.2332;
+    printf("%.2f",b);
+    char c='a';
+    printf("%c\n",c);
+    double d=10.2323;
+    printf("%d\n",d);
+    char s[] ="hello";
+    printf("%s\n",s);
+}
---
+++ program5.c
@@ -0,0 +1,13 @@
+# include<stdio.h>
+int main()
+{
+    int a=2;
+    int b=4;
+    int c=a+b;
+    int d=a-b;
+    int e=a/b;
+    int f=a*b;
+    int g=a%b;
+    printf("%d\n,%d\n,%d\n,%d\n,%d/n",c,d,e,f,g);
+    return 0;
+}
